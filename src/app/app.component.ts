f36fc3b93078175781ad39bf5f54f5f238a07036
hicham bel houcin <belhoucin.hicham@gmail.com>
2023-05-30 14:54:29 +0100

    Tue May 30 14:54:29 +01 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-project';
+  todolist: string[] = [];
+  value = '';
+
+  constructor() {
+    this.todolist = localStorage.getItem('todo')?.split(',') || [];
+  }
+
+  onKey(event: any) {
+    this.value = event.target.value;
+  }
+
+  addTask(task: string) {
+    // Check if the task is empty or already exists in the list
+    if (task.trim() === '' || this.todolist.includes(task)) {
+      return;
+    }
+
+    this.todolist.push(task);
+    localStorage.setItem('todo', this.todolist.join(','));
+    this.value = '';
+  }
+
+  deleteTask(task: string) {
+    this.todolist = this.todolist.filter((t) => t !== task);
+    localStorage.setItem('todo', this.todolist.join(','));
+  }
 }
